d530a646bd8b94b0c9716b17b6e41f9c18298a19
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2017-11-27 10:33:28 +0900

    add subscribe/unsubscribe feature

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var subscription = require('../lib/subscription');
 
 router.get('/', function(req, res, next) {
+  console.log('Getting current subscriptions...');
   subscription.list(function(err, subscriptions) {
     if (err) {
       return next(err);
@@ -12,6 +13,26 @@ router.get('/', function(req, res, next) {
       title: 'RSS reader - Subscriptions',
       subscriptions: subscriptions
     });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  subscription.subscribe(req.body.url, function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/subscriptions');
+  });
+});
+
+router.post('/:id/delete', function(req, res, next) {
+  var id = req.params.id;
+  console.log('Unsubscribing ' + id);
+  subscription.unsubscribe(id, function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/subscriptions');
   });
 });
 
